a1ec8eeb45720d0a5f996ccad7095e688ff2add0
aymen <ajroudi@et.esiea.fr>
2024-06-15 17:44:23 +0200

    Chatbot GPT

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import article_list, set_language_view
+from .views import article_list, chatbot_view, set_language_view
 
 urlpatterns = [
     path('article-list/', article_list, name="article-list"),
     path('set-language/', set_language_view, name='set_language'),
+    path('chatbot_view/', chatbot_view, name='chatbot_view'),
 ]
--- main/views.py
+++ main/views.py
@@ -2,8 +2,12 @@ from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from django.views.i18n import set_language
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 
+import openai, os
+from dotenv import load_dotenv
+load_dotenv() 
+api_key = os.getenv("OPENAI_API_KEY" , None)
 
 # Create your views here.
 
@@ -23,3 +27,23 @@ def set_language_view(request):
             return response
     # Gérer le cas où la méthode HTTP n'est pas POST ou si language est manquant
     return HttpResponse(status=400) 
+
+def chatbot_view(request):
+    chatbot_response = None
+    print(api_key)
+    if api_key is not None and request.method == 'POST':
+        print("in")
+        openai.api_key = api_key
+        user_input = request.POST.get('user_input')
+        prompt = user_input
+        response = openai.Completion.create(
+            engine ='gpt-3.5-turbo-instruct',
+            # prompt = prompt,
+            prompt = "Bonjour",
+            maw_tokens = 265,
+            temperature = 0.5
+        )
+        
+       
+        chatbot_response = response['choices'][0]['text']
+    return render(request, 'chatbot.html',{"response":chatbot_response})
